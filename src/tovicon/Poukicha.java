4d10ef4376d88aa2857668e7635c430f88d5d82c
Martin Raes <martin.raes.etu@univ-lille.fr>
2022-09-02 10:21:18 +0200

    ajout monstre  + attaque

---
+++ src/tovicon/Poukicha.java
@@ -0,0 +1,15 @@
+package tovicon;
+
+public class Poukicha extends Monster{
+	
+	static CoupDeQueue coupDeQueue ;
+	
+	public Poukicha(String name, String representation, Attack attacks) {
+		super("Poukicha", "Inserer asciiart", coupDeQueue);
+	}
+
+	
+
+
+
+}

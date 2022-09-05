5d9e88341eb1b79702773680d475757934a9b8a0
Clement Delerue <clement.delerue.etu@univ-lille.fr>
2022-09-05 10:18:01 +0200

    Ajout test arena

--- src/main/arenas/ArenaTest.java
+++ src/main/arenas/ArenaTest.java
@@ -5,38 +5,38 @@ import org.junit.jupiter.api.Test;
 import monsters.Monster;
 import monsters.Poukicha;
 
+import monsters.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 
 /**
- *  Cette classe permet de tester la classe Arena
- *  
+ * Cette classe permet de tester la classe Arena
+ * 
  * @author DELERUE Clement
  * @version 02/09/2022
  */
 class ArenaTest {
-	
+
 	/**
 	 * Ce test permet de tester le constructeur et les getteurs
 	 */
 	@Test
 	void testArena() {
-		//GIVEN
-		String name = "Arene 1" ;
+		// GIVEN
+		String name = "Arene 1";
 		ArrayList<Monster> listemonster = new ArrayList<Monster>();
-		
-		//WHEN
+
+		// WHEN
 		listemonster.add(new Poukicha());
 		Arena arenaTest = new Arena(name);
 		arenaTest.setActualMonster(new Poukicha());
-		
-		//THEN
+
+		// THEN
 		assertEquals(name, arenaTest.getName());
 		assertEquals(new Poukicha(), arenaTest.getActualMonster());
 		assertEquals(true, arenaTest.isDefeated());
-	}	
-	
+	}
 
 }
-
